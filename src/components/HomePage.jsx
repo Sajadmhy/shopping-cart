499955339c3592626190794ffae96d6f528a91f3
Sajadmhy <sajad.mahyaei@gmail.com>
2022-05-05 17:46:52 -0400

    Style HomePage component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,15 +1,21 @@
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import './style/HomePage.css';
 
 export default function HomePage() {
 
 return (
     <div className='container'>
         <div className="header">
-            <p>Sajad's Shop</p>
-            <p>Shop Now</p>
-            <div className='search'><SearchIcon/></div>
-            <div className='cart'><ShoppingCartIcon/></div>
+            <div className='item'><p>Book Store</p></div>
+            <div className='item'><p>Shop</p></div>
+            <div className='search item'><SearchIcon/></div>
+            <div className='cart item'><ShoppingCartIcon/></div>
+        </div>
+        <div className='body'>
+            <h2>Best Bookstore in the World</h2>
+            <p>Books available for preorder.</p>
+            <button>SHOP NOW</button>
         </div>
     </div>
 );
